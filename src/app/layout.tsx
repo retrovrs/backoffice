2e7450cd10dea27e50c7962719a4f3291216a069
gilbruno <gilles_bruno@gmx.fr>
2025-05-07 10:01:23 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "RetroVRS Backoffice",
   description: "RetroVRS Backoffice",
+  icons: {
+    icon: '/images/retrovrs_favicon.png',
+    shortcut: '/images/retrovrs_favicon.png',
+    apple: '/images/retrovrs_favicon.png',
+  },
 };
 
 export default function RootLayout({
